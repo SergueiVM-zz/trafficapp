39b45ffdbe3f69fefe0948bb17cfbdff7d4d65e4
Vicario Manjon, Serguei <s.vicario@libertyseguros.es>
2021-10-07 00:19:20 +0200

    Update road list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ export class AppComponent {
   trafficEvents: TrafficEvent[] = [];
   provinces: any[] = [];
   roads: any[] = [];
+
   filteredEvents: TrafficEvent[] = [];
+  filteredRoads: any[] = [];
 
   constructor(private infocarService: InfocarService, private availableProvinceService: AvailableProvincesService) { }
 
@@ -29,25 +31,54 @@ export class AppComponent {
           }
         });
 
-      this.roads = events.map(item => item.carretera)
-        .reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], [])
-        .map(road => {
-          return {
-            "name": road,
-            "value": true
-          }
-        });
+
+      this.roads = this.getAvailableRoads();
 
       this.filterTrafficEvents();
     });
 
   }
 
+  getActiveProvinces() {
+    return this.provinces.filter(item => item.value).map(item => item.name);
+  }
+
+  getAvailableRoads() {
+    const activeProvices = this.getActiveProvinces();
+    return this.trafficEvents
+      .filter(trafficEvent => activeProvices.includes(trafficEvent.provincia))
+      .map(item => item.carretera)
+      .reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], [])
+      .map(road => {
+        return {
+          "name": road,
+          "value": true
+        }
+      });
+  }
+
+  filterRoads(activeProvinces) {
+    const activeProvices = this.provinces.filter(item => item.value).map(item => item.name);
+
+  }
+
+  updateRoads() {
+
+    const availableRoads = this.getAvailableRoads();
+    const availableRoadsIndex = availableRoads.map(road => road.name);
+    //Remove Unavailable Roads
+    this.roads = this.roads.filter(road => availableRoadsIndex.includes(road.name));
+    //Add new roads
+    const roadsIndex = this.roads.map(road => road.name);
+    availableRoads.filter(road => !roadsIndex.includes(road.name)).forEach(road => this.roads.push(road));
+    console.log
+    return this.roads;
+  }
+
   filterTrafficEvents() {
     console.log("filterTrafficEvents");
-    
-    const activeProvices = this.provinces.filter(item => item.value).map(item => item.name);
-    const activeRoads = this.roads.filter(item => item.value).map(item => item.name);
+    const activeProvices = this.getActiveProvinces();
+    const activeRoads = this.updateRoads().map(item => item.name);
     this.filteredEvents = this.trafficEvents
       .filter(trafficEvent => activeProvices.includes(trafficEvent.provincia))
       .filter(trafficEvent => activeRoads.includes(trafficEvent.carretera));
