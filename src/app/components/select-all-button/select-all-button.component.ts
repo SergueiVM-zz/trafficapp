7ab1228b95454ff0d7f983531b03ab57f40c761b
Vicario Manjon, Serguei <s.vicario@libertyseguros.es>
2021-10-07 12:02:40 +0200

    Fix previous commit

---
+++ src/app/components/select-all-button/select-all-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-all-button',
+  templateUrl: './select-all-button.component.html',
+  styleUrls: ['./select-all-button.component.css']
+})
+export class SelectAllButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/swap-button/swap-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-swap-button',
+  templateUrl: './swap-button.component.html',
+  styleUrls: ['./swap-button.component.css']
+})
+export class SwapButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
